f4df8410d6984cfc800d95fefc7581e8f7fe6c23
JoshuaTian13 <82555700+JoshuaTian13@users.noreply.github.com>
2025-10-18 15:54:05 -0400

    Update opc.hpp

--- v3/src/lib/robot/util/opc.hpp
+++ v3/src/lib/robot/util/opc.hpp
@@ -1,87 +1,90 @@
+#ifndef __LIB_HPP__
+#define __LIB_HPP__
+
 #include "main.h"
 #include "pros/adi.hpp"
 #include "pros/misc.h"
-#include "pros/misc.hpp"
 #include "util.hpp"
 #include <vector>
+#include <functional>
 
+/**
+ * @file lib.hpp
+ * @brief Library of generic input abstractions and event listeners.
+ */
 
-namespace lib
-{
-    class digital
-    {
-        public:
-            virtual bool getState(){return true;};
+namespace lib {
+
+    /* ---------------- Base Digital ---------------- */
+    class digital {
+    public:
+        virtual bool getState() { return true; }
     };
 
-    class controllerButton : public digital
-    {
-        private:
-            pros::Controller con;
-            pros::controller_digital_e_t button;
+    /* ---------------- Controller Button ---------------- */
+    class controllerButton : public digital {
+    private:
+        pros::Controller con;
+        pros::controller_digital_e_t button;
 
-        public:
-            controllerButton(pros::Controller cont, pros::controller_digital_e_t key) : con(cont), button(key){}
+    public:
+        controllerButton(pros::Controller cont, pros::controller_digital_e_t key) 
+            : con(cont), button(key) {}
 
-            bool getState()
-            {
-                return(con.get_digital(button));
-            }
+        bool getState() override {
+            return con.get_digital(button);
+        }
     };
 
-    class limit : public digital
-    {
-        private:
-            pros::ADIDigitalIn sensor;
+    /* ---------------- Limit Switch ---------------- */
+    class limit : public digital {
+    private:
+        pros::ADIDigitalIn sensor;
 
-        public:
-            limit(pros::ADIDigitalIn limitSwitch) : sensor(limitSwitch) {}
+    public:
+        limit(pros::ADIDigitalIn limitSwitch) : sensor(limitSwitch) {}
 
-            bool getState()
-            {
-                return(sensor.get_value());
-            }
+        bool getState() override {
+            return sensor.get_value();
+        }
     };
 
-    struct action
-    {
+    /* ---------------- Action ---------------- */
+    struct action {
         lib::digital* binaryIn;
         std::function<void(void)> pressed;
         std::function<void(void)> unpressed;
 
-        action(lib::digital* digitalIn, std::function<void(void)> action, std::function<void(void)> defaultAction) : binaryIn(digitalIn), pressed(action), unpressed(defaultAction){}
+        action(lib::digital* digitalIn, 
+               std::function<void(void)> pressAction, 
+               std::function<void(void)> releaseAction)
+            : binaryIn(digitalIn), pressed(pressAction), unpressed(releaseAction) {}
     };
 
-    class listener
-    {
-        private:
-            std::vector<lib::action> actions;
+    /* ---------------- Listener ---------------- */
+    class listener {
+    private:
+        std::vector<lib::action> actions;
 
-        public:
-            listener(int a){}
-            listener(std::vector<lib::action> action) : actions(action){}
+    public:
+        listener(int) {}
+        listener(std::vector<lib::action> action) : actions(action) {}
 
-            void init(std::vector<lib::action> action)
-            {
-                actions = action;
-            }
+        void init(std::vector<lib::action> action) {
+            actions = action;
+        }
 
-            void listen()
-            {
-                for (int i = 0; i < actions.size(); i++)
-                {
-                    if(actions[i].binaryIn->getState())
-                    {
-                        actions[i].pressed();
-                    }
-
-                    else
-                    {
-                        actions[i].unpressed();
-                    }
+        void listen() {
+            for (auto &a : actions) {
+                if (a.binaryIn->getState()) {
+                    a.pressed();
+                } else {
+                    a.unpressed();
                 }
             }
+        }
     };
 
+} // namespace lib
 
-}   
+#endif
